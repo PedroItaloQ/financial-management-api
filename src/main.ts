f3353ea5af9e61efb541230d1bb17cdbaa287478
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-01-03 21:59:45 -0300

    feat: main.ts

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as session from 'express-session';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 }, // Duração da sessão: 1 hora
+    }),
+  );
+
+  await app.listen(3000);
+}
+bootstrap();
