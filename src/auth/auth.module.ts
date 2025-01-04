ba014beff6d292e6c23dec82fad273094f3c0430
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-01-03 22:00:09 -0300

    feat: Created authModule

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module'; // Importe o UsersModule aqui
+import { JwtStrategy } from '../auth/strategies/jwt.strategy';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './auth.controller'; // Importe o controlador
+
+console.log('MONGO_URI:', process.env.MONGO_URI); // Verifica a conexão com o banco
+console.log('SESSION_SECRET:', process.env.SESSION_SECRET); // Verifica a chave secreta
+
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.SESSION_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+    UsersModule,
+  ],
+  controllers: [AuthController], // Adicione o AuthController aqui
+  providers: [AuthService, JwtStrategy, LocalStrategy], // Inclua o LocalStrategy
+})
+export class AuthModule {}
