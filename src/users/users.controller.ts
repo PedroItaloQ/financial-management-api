c62acbb2da38b8f40ee1ed028e4ee255393d96c1
PedroItaloQ <peedroitalo@meirelesefreitas.adv.br>
2025-01-19 12:50:29 -0300

    feat: created user.controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from '../auth/strategies/jwt-auth.guard';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+}
